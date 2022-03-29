116f07736cd875bc912b6aa06dae6ea29bed5354
Ken Power <github@kenpower.com>
2022-03-29 08:50:58 +0100

    formatting

--- src/AssignBrief.svelte
+++ src/AssignBrief.svelte
@@ -8,7 +8,6 @@ export let reviewers = [];
 export let skills = [];
 export let skillsTree = [];
 
-
 let data = {};
 let brief_title
 let brief_link
@@ -16,103 +15,98 @@ let assignmentType;
 let selected_reviewers = [];
 let selected_learners = [];
 
-
 // let treedata = {
 // 	 text: "root",
-		
+
 // 	 items:[
 // 		 {
-// 			text: "test",
-// 			items: [
-// 				{ text: "subtest" },
-// 				{ text: "subtest2" },
-// 				{ text: "subtest3" },
-// 				{ text: "subtest4", 
-// 					items: [
-// 						{ text: "subtest" , selected: false },
-// 						{ text: "subtest2" },
-// 						{ text: "subtest3" },
-// 						{ text: "subtest4" },
-// 					]
-// 				},
-// 			]
+// 		text: "test",
+// 		items: [
+// 		{ text: "subtest" },
+// 		{ text: "subtest2" },
+// 		{ text: "subtest3" },
+// 		{ text: "subtest4", 
+// 		items: [
+// 		{ text: "subtest" , selected: false },
+// 		{ text: "subtest2" },
+// 		{ text: "subtest3" },
+// 		{ text: "subtest4" },
+// 		]
+// 		},
+// 		]
 // 		},
 // 		{
-// 			text: "test2",
-// 			items: [
-// 				{ text: "subtest" },
-// 				{ text: "subtest2" },
-// 				{ text: "subtest3" },
-// 				{ text: "subtest4" },
-// 			]
+// 		text: "test2",
+// 		items: [
+// 		{ text: "subtest" },
+// 		{ text: "subtest2" },
+// 		{ text: "subtest3" },
+// 		{ text: "subtest4" },
+// 		]
 // 		},
 // 		{
-// 			text: "test3",
-// 			items: [
-// 				{ text: "subtest" },
-// 				{ text: "subtest2" },
-// 				{ text: "subtest3" , selected: true},
-// 				{ text: "subtest4" },
-// 			]
+// 		text: "test3",
+// 		items: [
+// 		{ text: "subtest" },
+// 		{ text: "subtest2" },
+// 		{ text: "subtest3" , selected: true},
+// 		{ text: "subtest4" },
+// 		]
 // 		}
 // ]};
 
 let learner_names_sorted = []
 let reviewer_names_sorted = []
 
-$:{
-	learner_names_sorted = learners.sort();
-	reviewer_names_sorted = reviewers.sort();
+$: {
+    learner_names_sorted = learners.sort();
+    reviewer_names_sorted = reviewers.sort();
 }
-	
+
 function getChecked() {
-  var listOfTriples = [];
-	data = skills;
+    var listOfTriples = [];
+    data = skills;
 
-	Object.entries(data).forEach(area => 
- 		Object.entries(area[1].skills).forEach( skill => 
- 		   Object.entries(skill[1].checklistItems).forEach( checklistItem =>
-				{
-				console.log(checklistItem);
-			 	if(checklistItem[1].checked)
-			  	listOfTriples.push(checklistItem[1].checklist_item_triple);
-				}
-				)
-			)
-		)	
+    Object.entries(data).forEach(area =>
+        Object.entries(area[1].skills).forEach(skill =>
+            Object.entries(skill[1].checklistItems).forEach(checklistItem => {
+                console.log(checklistItem);
+                if (checklistItem[1].checked)
+                    listOfTriples.push(checklistItem[1].checklist_item_triple);
+            })
+        )
+    )
 
-  return listOfTriples;
+    return listOfTriples;
 }
-	
-function submit(){
+
+function submit() {
     let data = {}
 
-    data.author_email= "";
-    data.brief_title =brief_title;
+    data.author_email = "";
+    data.brief_title = brief_title;
     data.upload_url = brief_link;
-	
-    if(!assignmentType)
-    {
+
+    if (!assignmentType) {
         alert("You need to select a exemplar or brief type");
         return;
     }
 
-		data.brief_type = assignmentType;
+    data.brief_type = assignmentType;
     data.learners = selected_learners
     data.reviewers = selected_reviewers
 
     const selected_checklist_item_triples = getChecked();
 
-		if(selected_checklist_item_triples.length == 0) {
-					alert("You have to at least one skill selected!");
-					return;
-			}
+    if (selected_checklist_item_triples.length == 0) {
+        alert("You have to at least one skill selected!");
+        return;
+    }
 
     data.selected_checklist_item_triples = JSON.stringify(selected_checklist_item_triples);
 
     var submissionDatetime = document.getElementById("submission-datetime").value;
-    if(submissionDatetime == '')
-    {
+    if (submissionDatetime == '') {
         alert("You haven't set a submission date!");
         return;
     }
@@ -128,115 +122,114 @@ function submit(){
     data.final_deadline_time = time;
 
     console.log(data);
-//      fetch(app.baseUrl + '/assign_brief',
-//          {method:'POST',
-//              headers:{'Content-Type': 'application/json'},
-//              body: JSON.stringify(data)})
-//             .then(response => {
-//                 if (!response.ok) {
-//                     app.r = response;
-//                     response.text().then(t=>alert(t));
-//                     //alert(response);
-//                     console.log(response);
+    //      fetch(app.baseUrl + '/assign_brief',
+    //          {method:'POST',
+    //              headers:{'Content-Type': 'application/json'},
+    //              body: JSON.stringify(data)})
+    //             .then(response => {
+    //                 if (!response.ok) {
+    //                     app.r = response;
+    //                     response.text().then(t=>alert(t));
+    //                     //alert(response);
+    //                     console.log(response);
 
-//                 }
-//                 else
-//                 {
-//                     document.getElementById("assign-brief").style.display = "none";
-//                     document.getElementById("assign-brief-complete").style.display = "block";
+    //                 }
+    //                 else
+    //                 {
+    //                     document.getElementById("assign-brief").style.display = "none";
+    //                     document.getElementById("assign-brief-complete").style.display = "block";
 
-//                 }
-//                console.log(data);
-//             })
+    //                 }
+    //                console.log(data);
+    //             })
 }
 
 const firstDictItemKey = (dict) => Object.keys(dict)[0];
 
 const firstDictItem = (dict) => dict[firstDictItemKey(dict)];
-
-
-
 </script>
 
 <div>
     <div id = "assign-brief" class="outer_shell">
-			<form on:submit|preventDefault={submit}>
-			<section id="title">	
-			  	<label for="brief-title">Brief Title:</label>
-        	<input type="text" id="brief-title" class="brief_title" bind:value={brief_title} placeholder="Add a title for the brief" required>
-     </section>
-		 <section id="drive_link">	
-	   	<label for="attach-brief">Google Drive Link</label>
-  	  <div>
-						<input style="width:100%" type="text" bind:value={brief_link} name="attach-brief" id="attach-brief" required>
-						<div>Add a link to a Google drive folder shared with cleachtas@gmail.com, containing the brief.)</div>
-			</div>			
-		</section>
-  	<section id="learner">	
-					<label for="learner">Assign a learner:</label>
-        	<select name="learner" id="learners"  bind:value={selected_learners} multiple required>
-						{#each learner_names_sorted as learner}
-							<option>{learner}</option>
-						{/each}
-					</select>
-     </section>
-  	 <section id="reviewer">	
-					<label for="learner">Reviewers</label>
-        	<select name="reviewers" id="reviewers" bind:value={selected_reviewers} multiple required>
-						{#each reviewer_names_sorted as reviewer}
-							<option>{reviewer}</option>
-						{/each}
-					</select>
-     </section>
+        <form on:submit|preventDefault={submit}>
+            <section id="title">
+                <label for="brief-title">Brief Title:</label>
+                <input type="text" id="brief-title" class="brief_title" bind:value={brief_title} placeholder="Add a title for the brief" required>
+            </section>
+            <section id="drive_link">
+                <label for="attach-brief">Google Drive Link</label>
+                <div>
+                    <input style="width:100%" type="text" bind:value={brief_link} name="attach-brief" id="attach-brief" required>
+                    <div>Add a link to a Google drive folder shared with cleachtas@gmail.com, containing the brief.)</div>
+                </div>
+            </section>
+            <section id="learner">
+                <label for="learner">Assign a learner:</label>
+                <select name="learner" id="learners"  bind:value={selected_learners} multiple required>
+                    {#each learner_names_sorted as learner}
+                    <option>{learner}</option>
+                    {/each}
+                </select>
+            </section>
+            <section id="reviewer">
+                <label for="learner">Reviewers</label>
+                <select name="reviewers" id="reviewers" bind:value={selected_reviewers} multiple required>
+                    {#each reviewer_names_sorted as reviewer}
+                    <option>{reviewer}</option>
+                    {/each}
+                </select>
+            </section>
 
-			<section id="deadline">	
- 				 <label for="submission-datetime">Submission deadline:</label>
-         <input id="submission-datetime" type="datetime-local">
-      </section>
-      
-			<section id="assignment_type">	
-				 <label for="submission-datetime">Asignment Type:</label>
-					<div class = radios>
-						<label class="radio" for="exemplar">
-							<input type="radio" id="exemplar" name="brief_type" bind:group={assignmentType} value="exemplar"/>
-						Exemplar</label>
-						
-						<label class="radio" for="brief">
-							<input type="radio" id="brief" name="brief_type" bind:group={assignmentType} value="brief"/>
-							Brief</label>
-					</div>
-				</section>
-				<section id="checklist">	
-				 <label for="">Checklist items</label>
-					<CheckListSelector {skills}/>
-				</section>
-						<section id="checklist">	
-				 <label for="">Checklist items</label>
-					<TreeView branch = {skillsTree} itemClass={""} leafClass={""} selectedClass={"blue"}   expandIfDecendantSelected={true} >
-                    </TreeView>		
-				</section>
-   			<input id="submit" type="submit" value="Submit">
-				
-			</form>
-    </div>
-</div>
+            <section id="deadline">
+                <label for="submission-datetime">Submission deadline:</label>
+                <input id="submission-datetime" type="datetime-local">
+            </section>
+
+            <section id="assignment_type">
+                <label for="submission-datetime">Asignment Type:</label>
+                <div class = radios>
+                    <label class="radio" for="exemplar">
+                        <input type="radio" id="exemplar" name="brief_type" bind:group={assignmentType} value="exemplar"/>
+                        Exemplar</label>
+
+                    <label class="radio" for="brief">
+                        <input type="radio" id="brief" name="brief_type" bind:group={assignmentType} value="brief"/>
+                        Brief</label>
+                </div>
+            </section>
+            <section id="checklist">
+                <label for="">Checklist items</label>
+                <CheckListSelector {skills}/>
+                    </section>
+                    <section id="checklist">
+                        <label for="">Checklist items</label>
+                        <TreeView branch = {skillsTree} itemClass={""} leafClass={""} selectedClass={"blue"}   expandIfDecendantSelected={true} >
+                        </TreeView>
+                    </section>
+                    <input id="submit" type="submit" value="Submit">
+
+                    </form>
+                    </div>
+                    </div>
 
 <style>
-	section{
-		padding-bottom:20px;
-		display: flex;
-	}
-	section > label{
-		min-width: 20%;
-		max-width: 20%;
-	}
-	section > input, section > div{
-		flex-grow: 1;	
-	}
+section {
+    padding-bottom: 20px;
+    display: flex;
+}
 
-    		:global(.blue){
-			color:blue;
-			font-weight:700;
-		}
-	
+section>label {
+    min-width: 20%;
+    max-width: 20%;
+}
+
+section>input,
+section>div {
+    flex-grow: 1;
+}
+
+:global(.blue) {
+    color: blue;
+    font-weight: 700;
+}
 </style>
